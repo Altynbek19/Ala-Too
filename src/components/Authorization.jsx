fecf7ae7b1d37b9dd93c5180635ec91ca02266e4
ice-phoenix56 <mukayevk@bk.ru>
2023-12-14 02:34:22 +0600

    almost

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,82 +1,61 @@
 import React from 'react';
 import './Authorization.css'
-import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-const Authorization = (props) => {
-  const [login, setLogin] = useState('');
+const Authorization = () => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(false);
- const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-  async function signIn(e) {
+  const signIn = async (e) => {
     e.preventDefault();
+
+    // Простая валидация email
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      setError('Введите корректный email');
+      return;
+    }
+
+    // Замените на ваш реальный запрос для авторизации
     try {
-      setError(false);
+      // Ваш код для запроса авторизации
+      console.log('Успешная авторизация');
+      setError('');
 
-      // Проверка формата почты
-      const emailRegex = /\S+@\S+\.\S+/;
-      if (!emailRegex.test(login)) {
-        setError('Введенный e-mail не соответствует формату');
-        return;
-        
-      }
-     
-      // Замените этот код реальным запросом к вашему бэкенду
-      const response = await axios.post('http://85.209.9.201/api/auth/login', {
-        email: login,
-        password: password,
-      }, {
-        headers: {
-          Authorization: `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyMzE4MDExLCJpYXQiOjE3MDIyMzE2MTEsImp0aSI6IjE4MzkzM2U0NWUwOTQxZmJiZmQwY2ViNjdjOTU1YzJiIiwidXNlcl9pZCI6OH0.UJnPkeqcN6fZWh-Hd4owyXH0RL1yarkXpz5MzVhpZKA}`,
-        },
-      });
-
-      console.log('Успешный вход:', response.data);
-      // Добавьте код для обновления контекста аутентификации с полученными токенами
-      // Например: authContext.setTokens(response.data.accessToken, response.data.refreshToken);
-
-      // Переход на главную страницу
+      // Перенаправление на главную страницу
       navigate('/');
     } catch (error) {
-      setError(true);
-      console.error('Ошибка во время входа:', error);
+      console.error('Ошибка во время авторизации:', error);
+      setError('Ошибка входа. Пожалуйста, проверьте введенные данные.');
     }
-  }
+  };
 
   return (
-    <div className='cc'>
-      <div className="auth_box">
-        <div className="auth_reg">
-          <h1>Вход</h1>
+    <div>
+      <h2>Авторизация</h2>
+      <form onSubmit={signIn}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <form onSubmit={signIn}>
-          <div className='e-mail'>
-            <input
-              type="text"
-              placeholder='Введите e-mail'
-              value={login}
-              onChange={(e) => setLogin(e.target.value)}
-            />
-          </div>
-          <div className='password'>
-            <input
-              type="password"
-              name="password"
-              placeholder='Введите пароль'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {error && <span className="error" role="alert">{error}</span>}
-          <div className="Sign_in">
-            <button type="submit">Войти</button>
-          </div>
-        </form>
-        {/* ... остальной код ... */}
-      </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Войти</button>
+      </form>
     </div>
   );
 };
--- src/components/anxios.jsx
+++ src/components/anxios.jsx
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const registerUser = async (userData) => {
-  try {
-    const response = await axios.post('http://85.209.9.201/api/auth/register', userData);
-    console.log(response.data); // Обработайте ответ бэкенда
-  } catch (error) {
-    console.error('Error during registration:', error);
+const response = await axios.post(
+  'http://85.209.9.201/api/auth/login',
+  {
+    email: login,
+    password: password,
+  },
+  {
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyNTc3MzgyLCJpYXQiOjE3MDI0OTA5ODIsImp0aSI6ImNiMzQ3NjEyOWYyMDQzMDc4YWY0ODY5NDk3NTE3MDc1IiwidXNlcl9pZCI6MTF9.fa6t1ed8P5ODnDF9ZyA2Gh1tTVjNaeCOn2f46ZAXvGo`,
+    },
   }
-};
+);
